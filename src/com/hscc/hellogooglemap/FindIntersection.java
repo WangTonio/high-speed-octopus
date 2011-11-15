135bb25be223da17bbb76048bea8d5d56c0e70f7
alenbos@hotmail.com <alenbos@hotmail.com@defb89c6-3bb5-bd32-96c1-30d90c9cb4b6>
2011-11-15 05:17:09 +0000

    新增：FindIntersection 功能已完全Ported(將在下一版提出使用者介面，請稍等)

--- src/com/hscc/hellogooglemap/FindIntersection.java
+++ src/com/hscc/hellogooglemap/FindIntersection.java
@@ -1,16 +1,188 @@
 /* *** 以下使找路口的code實作 ****/
 package com.hscc.hellogooglemap;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+import com.hscc.hellogooglemap.HelloGoogleMapActivity.LandMarkOverlay;
 
 public class FindIntersection {
 	private static final int GEO = 1000000;
-	private static final int DIS = 1530;
+	private List<GeoPoint> passPoint = new ArrayList<GeoPoint>(); //記錄每一次向google取得的座標點
+	private Line myLine = null;
+	
 	GeoPoint taipei_station   = new GeoPoint( (int)(25.047192*GEO),(int)(121.516981*GEO));
 	
 	public GeoPoint findIntersec (GeoPoint beforeTurn1, GeoPoint beforeTurn2,GeoPoint afterTurn, int accurary){
     	GeoPoint intersec = null;
+    	GeoPoint nextDest = null;
+    	myLine = new Line(beforeTurn1 ,beforeTurn2, afterTurn);
+    	
+    	for(int i = 0; i < 10; i++)
+    	{
+    		nextDest = myLine.Function(i);
+    		GetDirection(beforeTurn1, nextDest);
+    		for(GeoPoint thistime : passPoint)
+    		{
+    			if ( (thistime != beforeTurn1) && (thistime != nextDest)){
+    				intersec = thistime;
+    			}
+    		}
+    	}
     	
     	return intersec;
     }
+	
+	
+/*  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&防害分隔線&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& */	
+	
+	
+	
+	/* *** 以下是 Google Path 要求 ****/
+	//The following function is made for PATH CALCULATION	
+	public List<GeoPoint> GetDirection(GeoPoint StartPoint ,GeoPoint Destination)
+	{
+	    String mapAPI = "http://maps.google.com/maps/api/directions/json?origin={0}&destination={1}&language=zh-TW&sensor=true";
+	    String url = MessageFormat.format(mapAPI, ExtraLocation(StartPoint),ExtraLocation(Destination));
+
+	    HttpGet get = new HttpGet(url);
+	    String strResult = "";
+	    try
+	    {
+	        HttpParams httpParameters = new BasicHttpParams();
+	        HttpConnectionParams.setConnectionTimeout(httpParameters, 3000);
+	        HttpClient httpClient = new DefaultHttpClient(httpParameters);
+
+	        HttpResponse httpResponse = null;
+	        httpResponse = httpClient.execute(get);
+
+	        if (httpResponse.getStatusLine().getStatusCode() == 200)
+	        {
+	            strResult = EntityUtils.toString(httpResponse.getEntity());
+
+	            JSONObject jsonObject = new JSONObject(strResult);
+	            JSONArray routeObject = jsonObject.getJSONArray("routes");
+	            String polyline = routeObject.getJSONObject(0).getJSONObject("overview_polyline").getString("points");
+
+	            if (polyline.length() > 0)
+	            {
+	                decodePolylines(polyline);
+	            }
+
+	        }
+	    }
+	    catch (Exception e)
+	    {
+	        Log.e("map", "MapRoute:" + e.toString());
+	    }
+
+	    return passPoint;
+	}
+
+	private String ExtraLocation(GeoPoint a){
+		String location = "";
+		location = "" + Double.toString(((double)a.getLatitudeE6())/GEO) + "," + Double.toString(((double)a.getLongitudeE6())/GEO);
+		return location;
+	}
+	
+	//Decode google path XML
+	private void decodePolylines(String poly)
+	{
+	    int len = poly.length();
+	    int index = 0;
+	    int lat = 0;
+	    int lng = 0;
+
+	    while (index < len)
+	    {
+	        int b, shift = 0, result = 0;
+	        do
+	        {
+	            b = poly.charAt(index++) - 63;
+	            result |= (b & 0x1f) << shift;
+	            shift += 5;
+	        } while (b >= 0x20);
+	        int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+	        lat += dlat;
+
+	        shift = 0;
+	        result = 0;
+	        do
+	        {
+	            b = poly.charAt(index++) - 63;
+	            result |= (b & 0x1f) << shift;
+	            shift += 5;
+	        } while (b >= 0x20);
+	        int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+	        lng += dlng;
+
+	        GeoPoint p = new GeoPoint((int) (((double) lat / 1E5) * 1E6), (int) (((double) lng / 1E5) * 1E6));
+	        passPoint.add(p);
+
+	    }
+	}
+	
+	/*
+	public class LandMarkOverlay extends ItemizedOverlay<OverlayItem>{
+
+		private List<OverlayItem> items = new ArrayList<OverlayItem>();
+		
+		public LandMarkOverlay(Drawable defaultMarker) {
+			super(defaultMarker);
+			//for(GeoPoint point : _points){
+			//	items.add(new OverlayItem(point, "HI", "這裡是路徑點"));
+			//}
+			items.add(new OverlayItem(taipei_station, "HI", "這裡是起點"));
+			items.add(new OverlayItem(my_destination, "HI", "這裡是終點"));
+			populate();
+		}
+		
+		@Override
+		protected OverlayItem createItem(int i){
+			return items.get(i);
+		}
+		
+		@Override
+		public int size(){
+			return items.size();
+		}
+		
+		@Override
+		protected boolean onTap(int pIndex){
+			Toast.makeText(HelloGoogleMapActivity.this, 
+						   "這裡是" + items.get(pIndex).getSnippet(), Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		
+	}
+
+	private void formMyPath() {
+		
+		Drawable pin = getResources().getDrawable(R.drawable.cluster);
+		pin.setBounds(0,0,pin.getMinimumWidth(),pin.getMinimumHeight());
+		
+		markLayer = new LandMarkOverlay(pin);
+		mapView.getOverlays().add(markLayer);
+	}
+
+	*/
+
 }
--- src/com/hscc/hellogooglemap/Line.java
+++ src/com/hscc/hellogooglemap/Line.java
@@ -1,24 +1,49 @@
-/* ノミ絬よ祘Α玂ㄤ把计称ら肚*/
+/* ノミ絬よ祘Α玂ㄤ把计称ら肚	 *
+ * latitude   琌絥 (X禸)				  	 *
+ * longitude  琌竒 (Y禸)					 */
+
 package com.hscc.hellogooglemap;
 
 import com.google.android.maps.GeoPoint;
 
 public class Line {
-	
+	private static final int DIS = 1530;
 	private double paraX = 0;
 	private double paraY = 0;
+	private double middleX = 0;
+	private double middleY = 0;
+	private double fixCoord = 0;
+	public GeoPoint myReturn = null;
 	
 	public Line(){
 		paraX = 30;
 		paraY = 30;
 	}
 	
+	/*      
+	 * (before2)よ ->
+	 *     〈△〈 (before1)
+	 *                	     
+	 *                            
+	 * (middle) 》             
+	 *                   
+	 *                
+	 *                 〈 (after)
+	 * 
+	 */
 	public Line(GeoPoint before1, GeoPoint before2, GeoPoint after){
-		
+		paraX = before1.getLatitudeE6()  - before2.getLatitudeE6();
+		paraY = before1.getLongitudeE6() - before2.getLongitudeE6();
+		middleX = (before1.getLatitudeE6() - after.getLatitudeE6() )/2;
+		middleY = (before1.getLongitudeE6()- after.getLongitudeE6())/2;
+		fixCoord = DIS / (Math.sqrt( paraX*paraX+paraY*paraY ));
 	}
 	
-	public double Function(double var){
-		
-		return 0;
+	public GeoPoint Function(double var){
+		int X,Y;
+		X = (int)(middleX + paraX * fixCoord * var);
+		Y = (int)(middleY + paraY * fixCoord * var);
+		myReturn = new GeoPoint(X,Y);
+		return myReturn;
 	}
 }
